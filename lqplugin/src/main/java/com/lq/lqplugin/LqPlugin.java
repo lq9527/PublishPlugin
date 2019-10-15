5738f699ea941c90f105a6abf336023293b65e42
liqiang32 <liqiang32@58ganji.com>
2019-10-15 17:10:22 +0800

    add module

---
+++ lqplugin/src/main/java/com/lq/lqplugin/LqPlugin.java
@@ -0,0 +1,12 @@
+package com.lq.lqplugin;
+
+/**
+ * @author liqiang
+ * @date 2019-10-15 17:08
+ * @des
+ */
+public class LqPlugin {
+    public static void hello() {
+        System.out.println("hello plugin");
+    }
+}
